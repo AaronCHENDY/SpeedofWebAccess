0c8a46843a28bd5122555993aa78eb93409296e8
AaronCHENDY <74288104+AaronCHENDY@users.noreply.github.com>
2020-11-11 16:09:39 +0800

    Update main.py
    add the UI to output the result

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from selenium import webdriver
 import os
 import datetime
 from tkinter import *
-
+allmessage=''
 
 def accessWeb(website):
     chromedriver = "C:\Program Files\Google\Chrome\Application\chromedriver.exe"
     os.environ["webdriver.chrome.driver"] = chromedriver
     driver = webdriver.Chrome(chromedriver)  # 模拟打开浏览器
-    starttime = datetime.datetime.now()
+
     try:
-        #website="http://www.decathlon.com.cn/"
+        starttime = datetime.datetime.now()
         driver.get(website)  # 打开网址
-    #driver.maximize_window()  # 窗口最大化（无关紧要哈）
+    #driver.maximize_window()  # 窗口最大化
         driver.close()
         endtime =datetime.datetime.now()
         consumetime=endtime-starttime
@@ -34,18 +34,18 @@ def avgaccess(website,trytimes):
     accesstime = []
     sum=0
     msg=""
-    singletxt = "访问网页 {0} 结果".format(website)
+    singletxt = "尝试访问 {0} {1}次，时间如下".format(website,trytimes)
     displaytxt(singletxt)
     for x in range(trytimes):
         accesstime.append(accessWeb(website))
         print("     第",x+1,"次打开网页",website,"时间为：",accesstime[x],"秒")
-        singletxt="\t\t 第 {0} 次时间为 {1} 秒".format(x+1,accesstime[x])
+        singletxt="\t\t 第 {0} 次耗时 {1} 秒".format(x+1,accesstime[x])
         displaytxt(singletxt)
         sum=sum+accesstime[x]
 
     print("平均打开网页",website,"时间为：",round((sum/trytimes),2),"秒")
     avgtime=round((sum/trytimes),2)
-    singletxt ="\n\t平均时间为 {0} 秒 ".format(avgtime)
+    singletxt ="\n\t平均耗时为 {0} 秒 ".format(avgtime)
     displaytxt(singletxt)
     return msg
 
@@ -56,20 +56,23 @@ def displaytxt(msg):
 
 
 
-allmessage=''
+
+
+def mainaction():
+
+    f=open('c:\\temp\web.txt','r')
+    websites=f.readlines()
+
+    for site in websites:
+        txt=(avgaccess(site,3))
+        displaytxt(txt)
+    Message(myWindow, text=allmessage, bg="#87CEEB", font=('Arial 12 bold'), width=600).pack()
+
 myWindow = Tk()
 myWindow.title('Websites Access Time')
 myWindow.geometry('800x600')
 
-
-f=open('c:\\temp\web.txt','r')
-websites=f.readlines()
-
-for site in websites:
-    txt=(avgaccess(site,2))
-    displaytxt(txt)
-
-Message(myWindow,text=allmessage, bg='gray', font=('Arial 12 bold'), width=800).pack()
+Message(myWindow,text="开始测速！", bg="#87CEEB", font=('Arial 12 bold'),width=600).pack()
+Button(text='提交', width=10,command=mainaction).pack()
 
 myWindow.mainloop()
-
